0c81a3603badfc1204091053ef561fc5d7fbecdf
Woods-Taylor <woods_taylor@columbusstate.edu>
2019-12-04 01:00:19 +0000

    completed step 2 merging for step 3

--- app.js
+++ app.js
@@ -5,10 +5,29 @@ var port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+
+var date = "";
+var explanation = "";
+var title = "";
+var url = "";
+
 app.get('/', function(req, res){
     myFetch()
     res.render("index", {img:img, year:year, title:title})
 });
+
+function getData(){
+    fetch('https://csuserversidewebdevfinal.herokuapp.com/')
+    .then(res => res.json())
+    .then(data => {
+        date = data.date;
+        explanation = data.explanation;
+        title = data.title;
+        url = data.url;
+    })
+
+}
+
 
 app.listen(port, function(){
 
